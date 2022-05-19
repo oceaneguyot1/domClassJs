b79eb5ea64d237c27e256a9efaf14ad03aca8fd6
John Doe <oceane.guyot@novei.fr>
2022-05-19 10:13:22 +0200

    foreach et map class name

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let divs = document.getElementsByClassName('jsClassSelector');
+let divs = document.getElementsByClassName('jsClassSelector'); //retourne une collection d'élements
 
-for(let i=0; i<divs.length; i++){
+
+for (let i = 0; i < divs.length; i++) {
     let divIndiv = divs[i];
 
     //Ne pas oublier de les supprimer au fur et à mesure
@@ -9,11 +10,29 @@ for(let i=0; i<divs.length; i++){
     console.log(divIndiv);
     console.log('----------');
 
-    if(i != 2){
+    if (i != 2) {
         divIndiv.classList.add('jsClassStyle');
     } else {
         divIndiv.style.color = "green";
     }
 
-    
+
 }
+
+
+let divsMap = Array.from(document.getElementsByClassName("jsClassSelector"));
+
+divsMap.map(element => {
+    element.classList.add('jsClassStyleMap');
+})
+
+
+let divForeach = document.querySelectorAll(".jsClassSelector"); //retourne une nodeList
+let divForeachIndiv = document.querySelector(".jsClassSelector"); //retourne une nodeList
+
+console.log(divForeach);
+console.log(divForeachIndiv);
+
+divForeach.forEach(element => {
+    element.classList.add('jsClassStyleForeach');
+})
